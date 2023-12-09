9cd23699e7db0140e7d83041cea8d631079a2f0e
fjorn-x <hamzashaikh.cs@gmail.com>
2023-12-09 13:02:43 +0530

    docker added

--- kennect-backend/server.js
+++ kennect-backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(errorHandler);
 app.use(express.urlencoded({extended: true}));
 
-mongoose.connect("mongodb://localhost:27017/kennect");
+mongoose.connect("mongodb://mongo-db/kennect");
 const db = mongoose.connection;
 db.on("connected", () => {
   console.log(`Database connected: ${db.host}`.cyan.underline);
